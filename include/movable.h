0d53f4bd5921dc4ac296ff69a867b32189969d5d
joswi <jsw94etik@yahoo.com>
2016-05-22 19:57:58 +0200

    Inital commit

---
+++ include/movable.h
@@ -0,0 +1,16 @@
+#ifndef MOVABLE_H
+#define MOVABLE_H
+
+
+class movable
+{
+    public:
+        movable();
+        virtual ~movable();
+
+    protected:
+
+    private:
+};
+
+#endif // MOVABLE_H
---
+++ src/movable.cpp
@@ -0,0 +1,14 @@
+#include "movable.h"
+class "movable"
+
+
+
+movable::movable()
+{
+    //ctor
+}
+
+movable::~movable()
+{
+    //dtor
+}
