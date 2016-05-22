fc6a6174014a65051d7fc45d80f68fecfe858797
joswi <jsw94etik@yahoo.com>
2016-05-22 22:01:30 +0200

    Movable Updated

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Movable m= new Movable(20, 10, 3, 5);
+System.out.println("Max is " + (Integer.MAX_VALUE+1));
+System.out.println("Min is " + (Integer.MIN_VALUE-1));
+
+
+	}
+
+}
--- src/Movable.java
+++ src/Movable.java
@@ -1,4 +1,27 @@
 
 public class Movable {
 
+	double x, y, velx, vely;
+	
+	
+	public Movable (double x, double y, double velx, double vely){
+		this.x=x;
+		this.y=y;
+		this.velx=velx;
+		this.vely=vely;
+		}
+	
+	public void update(){
+		System.out.println("xold="+x);
+		System.out.println("yold="+y);
+		this.x=velx+x;
+		this.y=vely+y;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		
+	}
+	
+	
+	
+	
 }
